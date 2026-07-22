57298d33e2a00c81f102128cebb693ddec4f3dcd
BlackHole <factory-admin@xyz-labs.local>
2026-07-21 21:52:32 -0400

    feat: add Alley executive landing page

---
+++ worker-wb/public/cj.js
@@ -0,0 +1,45 @@
+const saveBtn = document.getElementById("saveContact");
+
+const vcard = `BEGIN:VCARD
+VERSION:3.0
+N:Capobianco;CJ;;;
+FN:CJ Capobianco
+ORG:Blackhole Capital
+TITLE:Founder | AI Systems Architect
+TEL;TYPE=CELL:+1-617-901-6112
+EMAIL;TYPE=WORK:info@blackholecapital.xyz
+URL:https://blackholecapital.xyz
+URL:https://xyz-labs.xyz
+URL:https://github.com/blackholecapital
+URL:https://github.com/blackholecapital/Audits
+URL:https://git.xyz-labs.xyz/factory-admin/Audits
+URL:https://www.youtube.com/@xyz-Labs-xyz
+URL:https://x.com/Mktmakerxyz
+NOTE:Telegram @sparkie8675309
+END:VCARD`;
+
+saveBtn.onclick = () => {
+
+    const blob = new Blob(
+        [vcard],
+        { type: "text/vcard" }
+    );
+
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement("a");
+
+    a.href = url;
+    a.download = "CJ-Capobianco.vcf";
+
+    document.body.appendChild(a);
+
+    a.click();
+
+    a.remove();
+
+    URL.revokeObjectURL(url);
+
+};
+
+console.log("CJ Executive Page Loaded");
